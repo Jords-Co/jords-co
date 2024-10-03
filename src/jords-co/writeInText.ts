54a81928e2b38f88b32cff28327b98f3c68dd291
Digerati <cabal@digerati.design>
2024-10-03 21:58:28 +0100

    Remove green highlight skip white words

--- src/jords-co/writeInText.ts
+++ src/jords-co/writeInText.ts
@@ -20,7 +20,10 @@ export const writeInText = () => {
             position: 'relative'
         });
         const layoutTL = gsap.timeline();
+        /*
         const filteredWords = split.words?.filter((word) => !word.parentElement.classList.contains('text-color-white'));
+        */
+        const filteredWords = split.words;
         layoutTL.to(filteredWords, {
             color: '#3f8',
             stagger: 0.1,
