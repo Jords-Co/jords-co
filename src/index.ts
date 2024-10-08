b4bcb0cbdaa6798fd135518520ad053618b59f04
Digerati <cabal@digerati.design>
2024-10-08 19:52:45 +0100

    Add GSAP Flip hover state

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,10 @@
-import { formValidation } from "$digerati/formValidation";
 import { skipToMainContent } from "$digerati/skipToMainContent";
 import { currentYear } from "$digerati/currentYear";
 import { blurInElements } from "$jords-co/blurInElements";
 import { writeInText } from "$jords-co/writeInText";
 import { populateGrid } from "$jords-co/populateGrid";
 import { animateGrid } from "$jords-co/animateGrid";
+import { desktopMenuHoverState } from "$jords-co/desktopMenuHoverState";
 import { updateProjectInfo } from "$jords-co/updateProjectInfo";
 import { homeHeroAnimation } from "$jords-co/homeHeroAnimation";
 
@@ -12,10 +12,11 @@ window.Webflow || [];
 window.Webflow.push(() => {
   currentYear();
   skipToMainContent();
-  homeHeroAnimation();
+  desktopMenuHoverState();
   populateGrid();
   animateGrid();
-  updateProjectInfo();
+  homeHeroAnimation();
   blurInElements();
   writeInText();
+  updateProjectInfo();
 });
--- src/jords-co/desktopMenuHoverState.ts
+++ src/jords-co/desktopMenuHoverState.ts
@@ -1,3 +1,4 @@
+import { gsap } from 'gsap';
 import { Flip } from 'gsap/Flip';
 
 /**
@@ -6,14 +7,46 @@ import { Flip } from 'gsap/Flip';
  * @author <cabal@digerati.design>
  */
 export const desktopMenuHoverState = () => {
-    // const fxContainers = document.querySelectorAll('[dd-grid="fx-container"]');
-    // if (!fxContainers) {
-    //     return;
-    // }
-    // fxContainers.forEach((fxContainer) => {
-    //     for (let i = 0; i < 3000; i++) {
-    //         const fx = document.createElement('i');
-    //         fxContainer.appendChild(fx);
-    //     }
-    // });
+    const navLinks = document.querySelectorAll('.navbar2_link');
+    const navCorners = document.querySelector('.navbar2_link-hover');
+    if (!navLinks || !navCorners) {
+        return;
+    }
+    gsap.registerPlugin(Flip);
+    navLinks.forEach(function (link) {
+        /* Give active link a combo class after removing it from other links */
+        link.addEventListener('click', function () {
+            navLinks.forEach(function (link) {
+                link.classList.remove('is--active');
+            });
+            this.classList.add('is--active');
+        });
+        // ———— Move corners into link we're hovering
+        link.addEventListener('mouseenter', function () {
+            const inactiveLink = navCorners.classList.contains('is-inactive');
+            if (inactiveLink) {
+                navCorners.classList.remove('is-inactive');
+            }
+            const state = Flip.getState(navCorners);
+            this.appendChild(navCorners);
+            Flip.from(state, {
+                duration: 0.4,
+                ease: 'power1.inOut'
+            });
+        });
+        // ———— Move corners back to active link on hover out
+        link.addEventListener('mouseleave', function () {
+            const state = Flip.getState(navCorners);
+            const activeLink = document.querySelector('.navbar2_link.is--active');
+            if (!activeLink) {
+                navCorners.classList.add('is-inactive');
+                return;
+            }
+            activeLink.appendChild(navCorners);
+            Flip.from(state, {
+                duration: 0.4,
+                ease: 'power1.inOut'
+            });
+        });
+    });
 };
